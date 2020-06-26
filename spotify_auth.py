6156de6c5e35ab19654b13b91352b79ff0239f2c
SRocken <65569622+SRocken@users.noreply.github.com>
2020-06-25 22:30:21 -0400

    Made token auth specific to app functions

--- spotify_auth.py
+++ spotify_auth.py
@@ -1,34 +1,24 @@
-# Authenticate Spotify Users in order to use the app
-
+# Authenticate Spotify Users in order to use the app 
+ 
 import os
-import sys
 
 from dotenv import load_dotenv
 
 import spotipy
 import spotipy.util as util
-from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 
 load_dotenv()
 
-scope = 'user-library-read' # Scope sets access for the app in the authorized user's account
-username = input("Please input your username: ")
+# Define user token specific to this application
+scopes = 'user-follow-read playlist-modify-private user-read-playback-position' # Scopes set areas the app can access in the authorized user's account - only needs to be authorized once and then is saved in user's preferences
+username = input("Please input your username: ") #TODO: Make this part of the flask web app
 
 client_id_saved = os.environ.get("SPOTIFY_CLIENT_ID")
 client_secret_saved = os.environ.get("SPOTIFY_CLIENT_SECRET")
 redirect_uri_saved = os.environ.get("SPOTIFY_REDIRECT_URI")
 
 token = util.prompt_for_user_token(username,
-                        scope,
+                        scopes,
                         client_id= client_id_saved,
                         client_secret= client_secret_saved,
                         redirect_uri= redirect_uri_saved)
-
-if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print(track['name'] + ' - ' + track['artists'][0]['name'])
-else:
-    print("Can't get token for", username)
