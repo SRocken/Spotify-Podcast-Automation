0bc33c90f738244c0a14aa2ac1b7215a74c08ba4
cmoch <65452402+cmoch@users.noreply.github.com>
2020-06-27 09:56:24 -0400

    FInished output with new episode URI

--- Podcast_App/podcast_check.py
+++ Podcast_App/podcast_check.py
@@ -5,7 +5,7 @@ import spotipy
 import sys
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
-import json
+
 load_dotenv()
 import requests
 import datetime
@@ -55,61 +55,39 @@ for p in items:
     SHOW_LIST = [p["show"] for p in items]
     #print(SHOW_LIST)
 
+#Get Followed Show IDS
 for i in SHOW_LIST:
     ID_LIST = [i["id"] for i in SHOW_LIST]
 
-print(ID_LIST)
+#Compile episodes of Followed Shows
 episodes = []
-
 for x in ID_LIST:
     sodes = sp.show(x)
     episodes.append(sodes)
-#print(episodes)
 
+
+#recent_ep_uri = [ sub['uri'] for sub in recent_releases ] 
 
 #Podcast Desdcription 
-for q in episodes:
-    episode_info = [q['description'] for q in episodes]
-    #print(episode_info)
+#for q in episodes:
+#    episode_info = [q['description'] for q in episodes]
+#    #print(episode_info)
 
 
+#Remove Show Meta Data
 for q in episodes:
     episode_in = [q['episodes'] for q in episodes]
-    #print(episode_in[0])
-
 for q in episode_in:
     show_items = [q['items'] for q in episode_in]
-#print(show_items[])
 
-first = show_items[0]
-second = show_items[1]
-
-released_list = []
-
-for b in show_items:
-    releases = selected_pods = [b for b in first if str(b["release_date"]) == yesterday]
-    released_list.append(releases)
-
-print(released_list)
-#print(getList(first)) 
-
-#print(first)
+#Get Most Recent 
+recent_releases = [item[0] for item in show_items]
+recent_ep_uris = [ sub['uri'] for sub in recent_releases ] 
+print(recent_ep_uris)
 
 
 
-#for b in first:
-#    selected_pods = [b for b in first if str(b["release_date"]) == yesterday]
-#
-uri_list = []
-
-
-#for d in released_list:
-#    new_episode = [d["uri"] for d in released_list]
-#    uri_list.append(new_episode)
-#
-uris = uri_list
 
 
 
-#print(new_episode)
 
