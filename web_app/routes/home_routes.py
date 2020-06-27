b9ce0049f32f7de6dd0d2d37f98966b85c73a75d
SRocken <65569622+SRocken@users.noreply.github.com>
2020-06-27 10:32:29 -0400

    Linked auth function to web app

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -9,9 +9,79 @@ def index():
     print("Welcome to the Spotify Podcast Automation App")
     return render_template("login.html")
 
+
+@home_routes.route("/create", methods = ['GET', 'POST'])
+def Execute(username=None):
+    print("Executing login script")
+
+    username = request.args["username"]
+
+    username_detected = False
+
+    if(username):
+        username_detected = True
+
+    if(username_detected):
+        print("debug: " + username)
+        print("username detected")
+        #session['username'] = username
+
+        write_username_to_csv(username)
+
+        print("saved to memory")
+
+        auth_url = authenication_token(username).get_authorize_url()#> 'https://accounts.spotify.com/authorize?client_id=_____&response_type=code&redirect_uri=________&scope=playlist-modify-private+playlist-read-private'
+        print("redirect")
+        return redirect(auth_url)
+    else:
+        return render_template("no_token.html")
+
+    app.run(debug=True)
+
+@home_routes.route("/callback/")
+def Callback(code=None):
+
+    print("callback")
+    spotify_username = read_username_from_csv()
+
+    #delete user id from csv to maintain clean code
+    clear_username_csv()
+
+    #changed flow to accommodate changes of domain, resets session variable with expectation session will persist
+    session['username'] = spotify_username
+
+    print("get user id from csv")
+    print(spotify_username)
+
+    #gets authorization code from url
+    print("SPOTIFY CALLBACK")
+    print("REQUEST PARAMS:", dict(request.args))
+
+    if "code" in request.args:
+        code = request.args["code"]
+        print("CODE:", code)
+
+        sp_oauth = authenication_token(spotify_username)
+        token_info = sp_oauth.get_access_token(code)
+        print("TOKEN INFO:", token_info)
+        token = token_info["access_token"]
+        print("ACCESS TOKEN:", token)
+
+        session['token_var'] = token
+
+        check = check_login(token, spotify_username)
+        if(check):
+            print("Taking you to your podcast activity page")
+            return redirect("/activity")
+        else:
+            return render_template("no_token.html")
+    else:
+        message = "OOPS, UNABLE TO GET CODE"
+        print(message)
+        return message
+
+
 @home_routes.route("/activity")
 def activity():
     print("Visited Activity Page")
     return render_template("activity.html")
-
-# TODO: Change the login page to pull up the spotify login; with a rediret to the Activity page after login
--- web_app/spotify_auth.py
+++ web_app/spotify_auth.py
@@ -10,15 +10,19 @@ import spotipy.util as util
 load_dotenv()
 
 # Define user token specific to this application
-scopes = 'user-follow-read playlist-modify-private user-read-playback-position' # Scopes set areas the app can access in the authorized user's account - only needs to be authorized once and then is saved in user's preferences
-username = input("Please input your username: ") #TODO: Make this part of the flask web app
 
-client_id_saved = os.environ.get("SPOTIFY_CLIENT_ID")
-client_secret_saved = os.environ.get("SPOTIFY_CLIENT_SECRET")
-redirect_uri_saved = os.environ.get("SPOTIFY_REDIRECT_URI")
+def authenication_token(spotify_username):
+    scopes = 'user-follow-read playlist-modify-private user-read-playback-position' # Scopes set areas the app can access in the authorized user's account - only needs to be authorized once and then is saved in user's preferences
+    username = input("Please input your username: ") #TODO: Make this part of the flask web app
 
-token = util.prompt_for_user_token(username,
-                        scopes,
-                        client_id= client_id_saved,
-                        client_secret= client_secret_saved,
-                        redirect_uri= redirect_uri_saved)
+    client_id_saved = os.environ.get("SPOTIFY_CLIENT_ID")
+    client_secret_saved = os.environ.get("SPOTIFY_CLIENT_SECRET")
+    redirect_uri_saved = os.environ.get("SPOTIFY_REDIRECT_URI")
+
+    token = util.prompt_for_user_token(username,
+                            scopes,
+                            client_id= client_id_saved,
+                            client_secret= client_secret_saved,
+                            redirect_uri= redirect_uri_saved)
+    
+    return token
