ecd9a54831c5d276e34548090245ef7f53efb0a5
cmoch <65452402+cmoch@users.noreply.github.com>
2020-06-28 14:47:02 -0400

    Update functions to have empty list when no new releases

--- Podcast_App/functions_podcast_check.py
+++ Podcast_App/functions_podcast_check.py
@@ -45,11 +45,12 @@ def podcast_followed_new_eps(username, token):
     today = datetime.date.today()
     yesterday = str(today - datetime.timedelta(days=1))
     new_release = [b for b in recent_releases if str(b["release_date"]) == yesterday or date_today]
-    if len(new_release) == 0:
-        print("NONE")
-        exit()
-    recent_ep_uris = [ sub['id'] for sub in new_release] 
-    print(recent_ep_uris)
+    if len(new_release) > 0:
+        recent_ep_uris = [ sub['id'] for sub in new_release] 
+        print(recent_ep_uris)
+    else:
+        recent_ep_uris =[]
+        #print(recent_ep_uris)
 
 
 #podcast_followed_new_eps(username, token)
@@ -72,22 +73,21 @@ def new_ep_descriptions_titles(username, token):
     today = datetime.date.today()
     yesterday = str(today - datetime.timedelta(days=1))
     new_release = [b for b in recent_releases if str(b["release_date"]) == yesterday or date_today]
-    if len(new_release) == 0:
-        print("NONE")
-        exit()
-    recent_ep_uris = [ sub['uri'] for sub in new_release] 
-    new_episodes = []
-    for x in recent_ep_uris:
-        sodes = sp.episode(x)
-        new_episodes.append(sodes)
-    episode_info = ["PODCAST: " + q['show']["name"] + "\n" + "EPISODE: " + q['name'] + "\n"  + "DESCRIPTION: " + q['description'] +  "\n" + "LINK: " + q['external_urls']['spotify'] for q in new_episodes]
-    #print(*episode_info, sep = "\n" + "\n" + "\n")
-    print(*episode_info, sep = "\n" + "\n" + "\n" + "\n")
-
-
-
+    if len(new_release) > 0:
+        recent_ep_uris = [ sub['uri'] for sub in new_release] 
+        new_episodes = []
+        for x in recent_ep_uris:
+            sodes = sp.episode(x)
+            new_episodes.append(sodes)
+        episode_info = ["PODCAST: " + q['show']["name"] + "\n" + "EPISODE: " + q['name'] + "\n"  + "DESCRIPTION: " + q['description'] +  "\n" + "LINK: " + q['external_urls']['spotify'] for q in new_episodes]
+        #print(*episode_info, sep = "\n" + "\n" + "\n")
+        print(*episode_info, sep = "\n" + "\n" + "\n" + "\n")
+    else:
+        episode_info = []
 
 #new_ep_descriptions_titles(username, token)
+
+
 # IMAGE FUNCTION WORK IN PROGRESS 
 def new_ep_image_urls(username, token):
     sp = spotipy.Spotify(auth=token)
@@ -106,7 +106,7 @@ def new_ep_image_urls(username, token):
     yesterday = str(today - datetime.timedelta(days=1))
     new_release = [b for b in recent_releases if str(b["release_date"]) == yesterday or date_today]
     if len(new_release) == 0:
-        print("NONE")
+        recent_ep_uris =[]
         exit()
     recent_ep_uris = [ sub['uri'] for sub in new_release] 
     new_episodes = []
@@ -115,13 +115,6 @@ def new_ep_image_urls(username, token):
         new_episodes.append(sodes)
     episode_images_details = [q['images'] for q in new_episodes]
     #print(episode_images_details)
-    #episode_images = [q['url'] for q in episode_images_details]
-    #print(*episode_info, sep = "\n" + "\n" + "\n")
-    image_list = episode_images_details[0]
-    image_details = []
-    for q in image_list:
-        image2 = image_list[0]
-        image_details.append(image2) 
-    print(image_details)
-
-new_ep_image_urls(username, token)
+    single_urls = [b for b in episode_images_details if str(b["height"]) == "640"]
+    print(single_urls)
+#new_ep_image_urls(username, token)
