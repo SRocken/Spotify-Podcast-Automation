caab418528f5437f35bd9061eccf8e901facf7c1
cmoch <65452402+cmoch@users.noreply.github.com>
2020-06-30 17:33:02 -0400

    Updated Heroku

--- web_app/heroku.py
+++ web_app/heroku.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 import spotipy
 import spotipy.util as util
-from spotify_auth import authenication_token, read_username_from_csv
+from spotify_auth import authenication_token
 from playlist_creator import podcast_playlist_generator
 from playlist_management import podcast_followed_new_eps, user_playlist_add_episodes
 from email_update import send_episode_email
@@ -11,7 +11,7 @@ from email_update import send_episode_email
 load_dotenv()
 
 
-username = read_username_from_csv()
+username = os.getenv("username")
 
 token = authenication_token(username)
 
