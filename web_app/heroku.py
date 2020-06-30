7191c5cc74063924f15cce497439f370f9a5c06a
cmoch <65452402+cmoch@users.noreply.github.com>
2020-06-30 14:42:37 -0400

    Debugged Heroku

--- web_app/heroku.py
+++ web_app/heroku.py
@@ -2,13 +2,15 @@ import os
 from dotenv import load_dotenv
 
 import spotipy
-
+import spotipy.util as util
 from spotify_auth import authenication_token
 from playlist_creator import podcast_playlist_generator
 from playlist_management import podcast_followed_new_eps, user_playlist_add_episodes
 from email_update import send_episode_email
-
 load_dotenv()
+username = os.getenv("username")
+scope = 'user-library-read playlist-modify-public'
+
 #client_id_saved = os.environ.get("SPOTIFY_CLIENT_ID")
 #client_secret_saved = os.environ.get("SPOTIFY_CLIENT_SECRET")
 #redirect_uri_saved = os.environ.get("SPOTIPY_REDIRECT_URI")
@@ -20,8 +22,8 @@ load_dotenv()
 #                        client_id= client_id_saved,
 #                        client_secret= client_secret_saved,
 #                        redirect_uri= redirect_uri_saved)
+token = util.prompt_for_user_token(username, scope)
 
-username = os.getenv("username")
 
 authenication_token(username)
 
